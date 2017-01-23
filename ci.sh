bd216170bc0d3fe488b6e2d9b6fd5ee28adf7435
Douglas Beal <git@dougbeal.com>
2017-01-23 01:22:17 -0800

    add liquid tag to save slot time information to it can be reproduced in detail section

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@ this=$( cd $(dirname ${BASH_SOURCE[0]}); pwd -P )
 build_dir="$this/build"
 source_dir="$this"
 echo watch $source_dir
-files=$(find $source_dir -maxdepth 1 -name \*.md -or -name \*.markdown -or -name \*.json -or -name \*.liquid -or -name \*.sh  )
+files=$(find $source_dir -maxdepth 1 -name \*.md -or -name \*.markdown -or -name \*.json -or -name \*.liquid -or -name \*.sh -or -name \*.rb  )
 echo "files ${files}"
 bash $source_dir/build.sh | tee $build_dir/ci.log
 fswatch --print0 -otvx --batch-marker $files  $source_dir/build.sh --exclude \.#.* | xargs -0 -n 1 -I {} bash $source_dir/build.sh | tee $build_dir/ci.log
--- run.rb
+++ run.rb
@@ -17,6 +17,32 @@ module Liquid
   end
 end
 
+class SaveTime < Liquid::Tag
+  def initialize(tag_name, markup, tokens)
+    super
+    @variables = variables_from_string(markup)
+  end
+
+  def render(context)
+    #context.registers[:page]['slottime'] ||= Hash.new(0)
+    # context.stack do
+    #   slot = context.evaluate(@variables[0]).to_s
+    #   time = context.evaluate(@variables[1]).to_s
+    #   context.register[:page]['slottime'][slot] = time
+    #   time
+    # end
+    @variables[0].to_s
+  end
+
+  def variables_from_string(markup)
+    markup.split(',').collect do |var|
+      var =~ /\s*(#{Liquid::QuotedFragment})\s*/o
+      $1 ? Liquid::Expression.parse($1) : nil
+    end.compact
+  end  
+end
+Liquid::Template.register_tag('savetime', SaveTime)
+
 $stdout.write(
   Liquid::Cli.new(ARGV.first || '{}').render($stdin.read)
 )
