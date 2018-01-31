68fb5e4efc8376e613e01dfe735942dbee80b030
Douglas Beal <git@dougbeal.com>
2018-01-31 15:16:54 -0800

    include build_dir in wc, make sure build directory is supplied in ci.sh

--- ci.sh
+++ ci.sh
@@ -6,5 +6,5 @@ echo watch $source_dir
 files=$(find $source_dir -maxdepth 1 -name \*.md -or -name \*.markdown -or -name \*.json -or -name \*.liquid -or -name \*.sh -or -name \*.rb  )
 echo "files ${files}"
 bash $source_dir/build.sh build | tee $build_dir/ci.log
-fswatch --print0 -otvx --batch-marker $files  $source_dir/build.sh --exclude \.#.* | xargs -0 -n 1 -I {} bash $source_dir/build.sh | tee $build_dir/ci.log
+fswatch --print0 -otvx --batch-marker $files  $source_dir/build.sh --exclude \.#.* | xargs -0 -n 1 -I {} bash $source_dir/build.sh build | tee $build_dir/ci.log
 
